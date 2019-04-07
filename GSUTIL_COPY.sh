1be1bb83f71709df0d34cfa5019128f52dc2736f
Kyle MacIlvain <kyle.macilvain@berkeley.edu>
2019-04-06 20:50:42 -0400

    Quick fix

--- GSUTIL_COPY.sh
+++ GSUTIL_COPY.sh
@@ -3,6 +3,6 @@
 #Draws a sample of 5 images across 25 image folders
 for i in {1..25}; do
   for j in {1..5}; do
-    gsutil cp gs://w251-accident-detection/extracted_frames/extracted_frames/00000$i/10$j.jpg /Users/Kyle/Desktop/holding/extracted_frames/00000$i;
+    gsutil cp gs://w251-accident-detection/extracted_frames/extracted_frames/00000$i/10$j.jpg /Users/Kyle/Desktop/holding/extracted_frames/00000$i/10$j.jpg;
   done;
 done
